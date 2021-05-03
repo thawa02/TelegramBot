164d680772edc30862d63a067be9647e47e87b2d
thawa02 <71439209+thawa02@users.noreply.github.com>
2021-05-03 23:15:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import telebot
+from telebot import types
 import requests
 from bs4 import BeautifulSoup
-import tkn  #тут лежал токен
+import tkn
 
 
 bot = telebot.TeleBot(tkn.token)
@@ -9,42 +10,69 @@ bot = telebot.TeleBot(tkn.token)
 
 @bot.message_handler(commands=['start'])
 def send(message):
-    to_send = "Добро пожаловать! Я знаю много интересных фактов и хочу ими поделиться! Чтобы получить факт, пиши /fact"
-    bot.send_message(message.from_user.id, text=to_send)
+    to_send = "Добро пожаловать! Я знаю много интересных фактов и хочу ими поделиться! " \
+              "Чтобы получить факт, пиши /fact или нажми на кнопку"
+
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button = types.KeyboardButton(text="Получить факт")
+    keyboard.add(button)
+
+    bot.send_message(message.from_user.id, text=to_send, reply_markup=keyboard)
 
 
 @bot.message_handler(commands=['help'])
 def send(message):
-    bot.send_message(message.from_user.id, text="Чтобы получить факт, пиши /fact")
+    bot.send_message(message.from_user.id, text="Чтобы получить факт, пиши /fact или нажми на кнопку")
 
 
 @bot.message_handler(commands=['fact'])
 def send(message):
-    give_fact(message)
+    send_fact(message)
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+    if message.text == "Получить факт":
+        send_fact(message)
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
 
-def give_fact(message, curr=["nils-bor-i-dom-s-truboj-s-pivom/"]):
+def send_fact(message, curr=["nils-bor-i-dom-s-truboj-s-pivom/"]):
+    """Оправляет пользователю факт и картинку"""
+
     address = "https://zagge.ru/"
 
     r = requests.get(address + curr[0])
     soup = BeautifulSoup(r.text, 'html.parser')
-    for info in soup.select('#main'):
-        nxt = info.select('.more-facts')[0]
-        curr[0] = nxt.get('href')
+
+    fact = get_fact(soup)
+
+    bot.send_message(message.from_user.id, text=fact[0])
+    bot.send_photo(message.from_user.id, fact[1])
+
+    curr[0] = get_next_page(soup)
+
+
+def get_fact(soup):
+    """Возвращает текст факта и ссылку на картинку"""
 
     for info in soup.select('#main'):
         fact = info.select('.af-description')[0].text
         fact = fact.split("Больше фактов")[0]
+        fact = fact.split("Подробнее")[0]
         image = info.select('.af-image-ratio')[0].img
         src = image.get('src')
 
-    bot.send_message(message.from_user.id, text=fact)
-    bot.send_photo(message.from_user.id, src)
+    return [fact, src]
+
+
+def get_next_page(soup):
+    """Возвращает ссылку на страницу со следующим фактом"""
+
+    for info in soup.select('#main'):
+        nxt = info.select('.more-facts')[0]
+        return nxt.get('href')
 
 
 bot.polling(none_stop=True, interval=0)
